e5ca1c7d7ecf3ec7744fd193f8fed22cc334bca9
Kowa16 <owada1637@gmail.com>
2022-07-12 16:54:30 +0900

    ログイン機能の追加

--- src/db/schema.rb
+++ src/db/schema.rb
@@ -12,6 +12,7 @@
 
 ActiveRecord::Schema[7.0].define(version: 2022_07_09_002331) do
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "name"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
